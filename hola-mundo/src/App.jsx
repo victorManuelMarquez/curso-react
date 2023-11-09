93dbd20ae25fd368a453b42979ea122d25a5d18e
victor <victormmla3lm@gmail.com>
2023-11-09 18:23:59 -0300

    primer proyecto react

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <br />
+      <h1>¡¡¡Hola Mundo!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
